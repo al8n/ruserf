57c4e0e04073b8093f886bbafb291a6e077099d7
Al Liu <scygliu1@gmail.com>
2024-04-04 16:12:13 +0800

    Finish Query related unit tests

--- core/src/serf/base/tests/serf/query.rs
+++ core/src/serf/base/tests/serf/query.rs
@@ -121,6 +121,223 @@ where
   assert!(!s.should_process_query(&filters));
 }
 
+/// Unit test for serf query
+pub async fn serf_query<T>(transport_opts1: T::Options, transport_opts2: T::Options)
+where
+  T: Transport,
+{
+  let (event_tx, event_rx) = async_channel::bounded(4);
+
+  let s1 = Serf::<T>::with_event_sender(transport_opts1, test_config(), event_tx)
+    .await
+    .unwrap();
+
+  let wg = Arc::new(());
+  let wg1 = wg.clone();
+  let (ctx_tx, ctx_rx) = async_channel::bounded::<()>(1);
+  scopeguard::defer!(ctx_tx.close(););
+
+  <T::Runtime as RuntimeLite>::spawn_detach(async move {
+    let _wg = wg1;
+    loop {
+      futures::select! {
+        _ = ctx_rx.recv().fuse() => {
+          break;
+        },
+        e = event_rx.recv().fuse() => {
+          let e = e.unwrap();
+          match e.ty() {
+            EventType::Query => {
+              match e.kind() {
+                EventKind::Query(q) => {
+                  q.respond(Bytes::from_static(b"test")).await.unwrap();
+                  break;
+                }
+                _ => unreachable!(),
+              }
+            },
+            _ => continue,
+          }
+        },
+        _ = <T::Runtime as RuntimeLite>::sleep(Duration::from_secs(1)).fuse() => {
+          tracing::error!("timeout");
+          break;
+        },
+      }
+    }
+  });
+
+  let s2 = Serf::<T>::new(transport_opts2, test_config())
+    .await
+    .unwrap();
+
+  let serfs = [s1, s2];
+  wait_until_num_nodes(1, &serfs).await;
+
+  let node = serfs[1]
+    .advertise_node()
+    .map_address(MaybeResolvedAddress::resolved);
+  serfs[0].join(node, false).await.unwrap();
+
+  wait_until_num_nodes(2, &serfs).await;
+
+  // Start a query from s2;
+  let mut params = serfs[1].default_query_param().await;
+  params.request_ack = true;
+  let resp = serfs[1]
+    .query("load", Bytes::from_static(b"sup girl"), Some(params))
+    .await
+    .unwrap();
+
+  let mut acks = vec![];
+  let mut responses = vec![];
+
+  let ack_rx = resp.ack_rx().unwrap();
+  let resp_rx = resp.response_rx();
+
+  for _ in 0..3 {
+    futures::select! {
+      a = ack_rx.recv().fuse() => {
+        let a = a.unwrap();
+        acks.push(a);
+      },
+      r = resp_rx.recv().fuse() => {
+        let r = r.unwrap();
+        assert_eq!(r.from, serfs[0].advertise_node());
+        assert_eq!(r.payload, Bytes::from_static(b"test"));
+        responses.push(r);
+      },
+      _ = <T::Runtime as RuntimeLite>::sleep(Duration::from_secs(1)).fuse() => {
+        panic!("timeout");
+      },
+    }
+  }
+
+  assert_eq!(acks.len(), 2, "missing acks {acks:?}");
+  assert_eq!(responses.len(), 1, "missing responses {responses:?}");
+}
+
+/// Unit test for serf query filter
+pub async fn serf_query_filter<T>(
+  transport_opts1: T::Options,
+  transport_opts2: T::Options,
+  transport_opts3: T::Options,
+) where
+  T: Transport,
+{
+  let (event_tx, event_rx) = async_channel::bounded(4);
+
+  let s1 = Serf::<T>::with_event_sender(transport_opts1, test_config(), event_tx)
+    .await
+    .unwrap();
+
+  let wg = Arc::new(());
+  let wg1 = wg.clone();
+  let (ctx_tx, ctx_rx) = async_channel::bounded::<()>(1);
+  scopeguard::defer!(ctx_tx.close(););
+
+  <T::Runtime as RuntimeLite>::spawn_detach(async move {
+    let _wg = wg1;
+    loop {
+      futures::select! {
+        _ = ctx_rx.recv().fuse() => {
+          break;
+        },
+        e = event_rx.recv().fuse() => {
+          let e = e.unwrap();
+          match e.ty() {
+            EventType::Query => {
+              match e.kind() {
+                EventKind::Query(q) => {
+                  q.respond(Bytes::from_static(b"test")).await.unwrap();
+                  break;
+                }
+                _ => unreachable!(),
+              }
+            },
+            _ => continue,
+          }
+        },
+        _ = <T::Runtime as RuntimeLite>::sleep(Duration::from_secs(1)).fuse() => {
+          tracing::error!("timeout");
+          break;
+        },
+      }
+    }
+  });
+
+  let s2 = Serf::<T>::new(transport_opts2, test_config())
+    .await
+    .unwrap();
+
+  let mut serfs = vec![s1, s2];
+  wait_until_num_nodes(1, &serfs).await;
+
+  let node = serfs[1]
+    .advertise_node()
+    .map_address(MaybeResolvedAddress::resolved);
+  serfs[0].join(node, false).await.unwrap();
+
+  wait_until_num_nodes(2, &serfs).await;
+
+  let s3 = Serf::<T>::new(transport_opts3, test_config())
+    .await
+    .unwrap();
+  serfs.push(s3);
+
+  wait_until_num_nodes(1, &serfs[2..]).await;
+
+  let node = serfs[2]
+    .advertise_node()
+    .map_address(MaybeResolvedAddress::resolved);
+  serfs[0].join(node, false).await.unwrap();
+
+  wait_until_num_nodes(3, &serfs).await;
+
+  // Filter to only s1!
+  let mut params = serfs[1].default_query_param().await;
+  params.filters.push(Filter::Id(
+    [serfs[0].memberlist().local_id().clone()]
+      .into_iter()
+      .collect(),
+  ));
+  params.request_ack = true;
+  params.relay_factor = 1;
+
+  // Start a query from s2;
+  let resp = serfs[1]
+    .query("load", Bytes::from_static(b"sup girl"), Some(params))
+    .await
+    .unwrap();
+
+  let mut acks = vec![];
+  let mut responses = vec![];
+
+  let ack_rx = resp.ack_rx().unwrap();
+  let resp_rx = resp.response_rx();
+
+  for _ in 0..2 {
+    futures::select! {
+      a = ack_rx.recv().fuse() => {
+        let a = a.unwrap();
+        acks.push(a);
+      },
+      r = resp_rx.recv().fuse() => {
+        let r = r.unwrap();
+        assert_eq!(r.from, serfs[0].advertise_node());
+        assert_eq!(r.payload, Bytes::from_static(b"test"));
+        responses.push(r);
+      },
+      _ = <T::Runtime as RuntimeLite>::sleep(Duration::from_secs(1)).fuse() => {
+        panic!("timeout");
+      },
+    }
+  }
+
+  assert_eq!(acks.len(), 1, "missing acks {acks:?}");
+  assert_eq!(responses.len(), 1, "missing responses {responses:?}");
+}
+
 /// Unit test for serf query deduplicate
 pub async fn serf_query_deduplicate<T>(transport_opts: T::Options)
 where
