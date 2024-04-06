5593495a60f7e4d7b505106f0ed1bb64cb254503
Al Liu <scygliu1@gmail.com>
2024-04-06 22:22:58 +0800

    WIP: quinn unit tests

--- ruserf/tests/main/quic.rs
+++ ruserf/tests/main/quic.rs
@@ -1,1 +1,48 @@
 
+#[path = "./quic/delegate.rs"]
+mod delegate;
+
+#[path = "./quic/leave.rs"]
+mod leave;
+
+#[path = "./quic/reap.rs"]
+mod reap;
+
+#[path = "./quic/reconnect.rs"]
+mod reconnect;
+
+#[path = "./quic/remove.rs"]
+mod remove;
+
+#[path = "./quic/snapshot.rs"]
+mod snapshot;
+
+#[path = "./quic/update.rs"]
+mod update;
+
+#[path = "./quic/role.rs"]
+mod role;
+
+#[path = "./quic/set_tags.rs"]
+mod set_tags;
+
+#[path = "./quic/get_queue_max.rs"]
+mod get_queue_max;
+
+#[path = "./quic/local_member.rs"]
+mod local_member;
+
+#[path = "./quic/num_nodes.rs"]
+mod num_nodes;
+
+#[path = "./quic/state.rs"]
+mod state;
+
+#[path = "./quic/stats.rs"]
+mod stats;
+
+#[path = "./quic/coordinates.rs"]
+mod coordinates;
+
+#[path = "./quic/name_resolution.rs"]
+mod name_resolution;
